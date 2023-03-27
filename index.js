4053259d94cedd1af87645b635f5e5cb5e81bf6a
jupa005 <antelapad1@gmail.com>
2023-03-27 17:43:34 +0200

    test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //cc
 
 const connection = require("./connection");
 const { cookieJwtAuth } = require("./middleware/cookieJwtAuth");
